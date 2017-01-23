84c8e91713f85449b49b63a71fae0605ee047229
William A Sims III <willsims14@gmail.com>
2017-01-23 15:13:42 -0600

    Loaded songs from first JSON file into the DOM, also added 'more songs' feature.

--- javascripts/songs.js
+++ javascripts/songs.js
@@ -1,6 +1,27 @@
 var songs = [];
 var songsObj = [];
 
+// Get song card container
+var songsContainer = document.getElementById("main");
+
+// Remove only child of the container
+var songElement = songsContainer.removeChild(document.getElementById("song--0"));
+
+var dataRequest = new XMLHttpRequest();
+var dataRequest2 = new XMLHttpRequest();
+
+dataRequest.addEventListener("load", dataRequestComplete);
+dataRequest.addEventListener("error", dataRequestFailed);
+
+dataRequest2.addEventListener("load", dataRequestComplete);
+dataRequest2.addEventListener("error", dataRequestFailed);
+
+document.getElementById("moreButton").addEventListener("click", function(){
+	// Open and Send the XMLHttpRequest
+	dataRequest2.open("GET", "music2.json");
+	dataRequest2.send();
+})
+
 songs[songs.length] = "Estimated Prophet > by The Grateful Dead on the album Terrapin Station";
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
@@ -31,44 +52,86 @@ for(var i = 0; i < songs.length; i++){
 
 
 
-// Get song card container
-var songsContainer = document.getElementById("main");
-// Remove only child of the container
-var songElement = songsContainer.removeChild(document.getElementById("song--0"));
+// Populate the DOM
+populateDOM();
 
-console.log("Element: ", songElement.innerHTML);
-
-for(var i = 0; i < songs.length; i++){
-
-	// Get HTML content of the song card
-	var songHtmlStructure = songElement.innerHTML;
-
-	// Replace placeholder names with values from array of objects
-	songHtmlStructure = songHtmlStructure.replace("SONG_NAME", songsObj[i].name);
-	songHtmlStructure = songHtmlStructure.replace("SONG_ARTIST", songsObj[i].artist + "		|	");
-	songHtmlStructure = songHtmlStructure.replace("SONG_GENRE", "Genre" + "		|	");
-	songHtmlStructure = songHtmlStructure.replace("SONG_ALBUM", songsObj[i].album);
-	songHtmlStructure = songHtmlStructure.replace("</ul>", "</ul><br>");
-	songHtmlStructure = songHtmlStructure.replace(`<ul class="songInfo">`, `<ul class="songInfo container">`)
-	songHtmlStructure = songHtmlStructure.replace("--!", "--" + i);
-
-	console.log("struc: \n", songHtmlStructure);
-
-	var newSong = `<div class="song" id="song--${i}">`;
-	newSong += songHtmlStructure;
-	newSong += "<div>";
-
-	songsContainer.innerHTML += newSong;
+// Open and Send the XMLHttpRequest for music from ONLY the music.json file
+//		- music2.json is not loaded until "more" button is clicked
+dataRequest.open("GET", "music.json");
+dataRequest.send();
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function populateDOM(){
+	// If the songs container has children (songs) in it, delete them
+	if(songsContainer.hasChildNodes()){
+		while(songsContainer.firstChild){
+			songsContainer.removeChild(songsContainer.firstChild);
+		}
+	}
+	for(var i = 0; i < songsObj.length; i++){
+		// Get HTML content of the song card
+		var songHtmlStructure = songElement.innerHTML;
+
+		// Replace placeholder names with values from array of objects
+		songHtmlStructure = songHtmlStructure.replace("SONG_NAME", songsObj[i].name);
+		songHtmlStructure = songHtmlStructure.replace("SONG_ARTIST", songsObj[i].artist);
+		songHtmlStructure = songHtmlStructure.replace("SONG_GENRE", "Genre");
+		songHtmlStructure = songHtmlStructure.replace("SONG_ALBUM", songsObj[i].album);
+		songHtmlStructure = songHtmlStructure.replace("</ul>", "</ul><br>");
+		songHtmlStructure = songHtmlStructure.replace(`<ul class="songInfo">`, `<ul class="songInfo container">`)
+		songHtmlStructure = songHtmlStructure.replace("--!", "--" + i);
+
+		var newSong = `<div class="song" id="song--${i}">`;
+		newSong += songHtmlStructure;
+		newSong += "<div>";
+
+		songsContainer.innerHTML += newSong;
+	}
+	// Add event listeners to every delete button for each song
+	for(var i = 0; i < songsObj.length; i++){
+		document.getElementById("deleteButton--" + i).addEventListener("click", function(event){
+			deleteSong(event.target.id);
+		});
+	};
 }
 
-// Add event listeners to every delete button for each song
-for(var i = 0; i < songs.length; i++){
-	document.getElementById("deleteButton--" + i).addEventListener("click", function(event){
-		deleteSong(event.target.id);
-	});
-}
+function dataRequestComplete(event){
+	// Get array of new songs from JSON load event
+	var newSongs = JSON.parse(event.target.responseText);
+
+	// Add each new song to existing array of song objects
+	for(var i = 0; i < newSongs.length; i++){
+		songsObj.push(newSongs[i]);
+	}
+
+	// Repopulate the DOM
+	populateDOM();
+};
+
+function dataRequestFailed(event){
+	console.log("FAILED");
+};
+
 function deleteSong(songID){
 	var index = parseInt(songID.substring(14, songID.length));
 	var allSongs = document.getElementById("main");
@@ -83,16 +146,4 @@ function deleteSong(songID){
 
 	// Delete song from DOM
 	songToDelete.parentNode.removeChild(songToDelete);
-}
-
-
-
-    // <div id="song--0" class="song">
-    //   <h2 class="songTitle">SONG_NAME</h2><br>
-    //   <ul class="songInfo">
-    //     <li class="col-sm-12 col-md-2">SONG_ARTIST</li>
-    //     <li class="col-sm-12 col-md-2">SONG_GENRE</li>
-    //     <li class="col-sm-12 col-md-2">SONG_ALBUM</li>
-    //     <li class="col-sm-12 col-md-2"><input type="button" id="deleteButton" value="delete"></li>
-    //   </ul>
-    // </div>
+};
